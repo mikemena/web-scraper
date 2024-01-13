b75925e4535055279a07f253947f2787f54ed458
tokyo <tokyolabs@protonmail.com>
2024-01-13 08:41:12 -0500

    wip

--- web_scraping.py
+++ web_scraping.py
@@ -16,12 +16,14 @@ print(soup.select("title")[0].getText())
 # count paragraphs
 print(len(soup.select("p")))
 
+# Get element with a specified class
+stock_name = soup.select('[class="D(ib) Fz(18px)"]')[0].get_text()
+print(stock_name)
 
 # Get element with a specified attribute
 current_price = soup.select('[data-test="qsp-price"]')[0].get_text()
 print(current_price)
 
-
-# Get element with a specified class
-stock_name = soup.select('[class="D(ib) Fz(18px)"]')[0].get_text()
-print(stock_name)
+# Get element with a specified attribute
+price_range = soup.select('[data-test="FIFTY_TWO_WK_RANGE-value"]')[0].get_text()
+print(price_range)
